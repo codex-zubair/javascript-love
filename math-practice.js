ed436e1c9faa57ef72174da20f4779973b315e84
Rocko <codingwithrock@gmail.com>
2022-08-02 00:40:57 +0600

    formater fixed

---
+++ math-practice.js
@@ -0,0 +1,58 @@
+
+
+
+// array assign.
+var fruits = ["apple", "banana", "orange"];
+console.log(fruits);
+
+
+
+// adding mango
+console.log(fruits[1]);
+fruits[1] = "mango";
+console.log(fruits);
+
+// popping orange
+fruits.pop();
+console.log(fruits)
+
+
+// added watermelon
+fruits.push("Watermelon")
+console.log(fruits)
+
+
+
+//* Practice two
+
+var mark = 10;
+
+
+
+if (mark >= 80) {
+    console.log("A grade")
+}
+
+else if (mark >= 70) {
+    console.log("B grade")
+}
+
+else if (mark >= 50) {
+    console.log("C grade")
+}
+else if (mark >= 40) {
+    console.log("D grade")
+}
+
+else if (mark = 40) {
+    console.log("failed")
+}
+
+else {
+    console.log("working")
+}
+
+
+
+
+
