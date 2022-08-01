20b2a7c5183c168eff50c0ad7ba76116307f9423
Rocko <codingwithrock@gmail.com>
2022-08-01 13:09:08 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+
+// TODO I LOVE YOU JAVASCRIPT
+//* I LOVE YOU JAVASCRIPT
+
+console.log("Hello my love javascript")
+
+
+
+var book = "yes book";
+var apple = "five pice";
+var logo = "yes it's logo";
+
+
+
+
+console.log(book,apple,logo)
