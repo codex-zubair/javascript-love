4108c4245af03445232bbce40ffc701f82df5a74
Rocko <codingwithrock@gmail.com>
2022-08-01 15:33:39 +0600

    variable clear

--- variables.js
+++ variables.js
@@ -11,4 +11,39 @@ var apple = "five pice";
 var logo = "yes it's logo";
 
 
-console.log(book,apple,logo)
+console.log(book, apple, logo)
+
+
+
+
+// Number type variables
+var number = 123;
+
+// string type variable
+var book = parseInt(123.34343);
+
+
+// boolean type variables
+var isItTruth = true;
+
+// Adding Number 
+number += 100;
+
+
+// Converting variables
+console.log(typeof (parseInt(book)))
+console.log(typeof (String(book)))
+// Float
+console.log(book)
+
+
+
+
+
+
+
+
+
+
+
+
