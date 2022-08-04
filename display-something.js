d7c91d037c1eda044b805dbcbb759d7361970f71
Rocko <codingwithrock@gmail.com>
2022-08-04 11:21:54 +0600

    i love you javaScript 412 to 556 time

--- display-something.js
+++ display-something.js
@@ -12,17 +12,40 @@
 
 // Display something 58 to 98
 
-for (i = 1; i <= 100; i++) {
-    if (i < 58) {
+// for (i = 1; i <= 100; i++) {
+//     if (i < 58) {
+//         continue
+//     }
+
+//     console.log("i love you javaScript " + i);
+
+//     if(i==98)
+//     {
+//         console.log("loop done");
+//         stop
+//     }
+
+// }
+
+
+// display all even number 412 to 556
+
+
+for (i = 0; i <= 600; i += 2) {
+    if (i < 412) {
         continue
     }
 
-    console.log("i love you javaScript " + i);
-    
-    if(i==98)
-    {
-        console.log("loop done");
+
+
+
+    if (i <= 556) {
+        console.log(i + " I love you JavaScript");
+    }
+
+    if (i == 556) {
         stop
     }
+
 
 }
