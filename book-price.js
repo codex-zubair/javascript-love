3abcce5dc04fb1208a4e04cc9e7ba8e375c3e37a
Rocko <codingwithrock@gmail.com>
2022-08-04 21:10:51 +0600

    done section

---
+++ book-price.js
@@ -0,0 +1,18 @@
+const book = ["bookAbul=200", "bookKalam=300", "bookJobbar=150", "bookRoshid=40"]
+
+
+
+for (i = 0; i < book.length; i++) {
+
+
+
+    if (book[i] == "bookJobbar=150" || book[i] == "bookRoshid=40") {
+
+        console.log(book[i]);
+    }
+
+
+}
+
+
+
