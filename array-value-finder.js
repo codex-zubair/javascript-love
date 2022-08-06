030a360412740451e296227fd4cb87578c6dc8c4
Rocko <codingwithrock@gmail.com>
2022-08-06 14:56:54 +0600

    used of includes, shift, unshift, push, pop, of array elements

---
+++ array-value-finder.js
@@ -0,0 +1,19 @@
+arrayList = ["mango", "water", "book", "fruit"];
+
+
+console.log(arrayList);
+
+arrayList.unshift('un-shifted');
+
+
+console.log(arrayList);
+
+
+arrayList.push("push");
+
+
+console.log(arrayList);
+
+
+// search array value using includes
+console.log(arrayList.includes("mango"))
