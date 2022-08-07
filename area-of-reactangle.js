4388e8a5681e7f4483690cabeff5704000909215
Rocko <codingwithrock@gmail.com>
2022-08-07 10:41:19 +0600

    rectangle are formula function are ready

---
+++ area-of-reactangle.js
@@ -0,0 +1,21 @@
+// Length = 15 cm
+
+// Width = 4 cm
+
+// Area of a rectangle = Length × Width
+
+// 15 × 4  = 60
+
+// So the area of rectangle = 60 cm2
+
+
+
+
+function area(l, w) {
+    console.log("Rectangle Length: " + l + "cm" + " Rectangle Width: " + w + "cm")
+    console.log("area of rectangle: " + l * w + "cm2");
+}
+
+
+
+area(15, 4);
